32a4b4de293e1b5dcec71bee2de3c8ed031c7b71
Paulina Huerta <paulina.huerta0156@alumnos.udg.mx>
2026-04-24 00:35:59 -0600

    Avance 1: Sensor de color funcionando

---
+++ main/main.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
+
+// Pines LEDs
+#define LED_R GPIO_NUM_5
+#define LED_G GPIO_NUM_17
+#define LED_B GPIO_NUM_18
+
+// ADC
+#define ADC_CHANNEL ADC1_CHANNEL_4
+
+void app_main(void)
+{
+    // Configurar LEDs como salida
+    gpio_set_direction(LED_R, GPIO_MODE_OUTPUT);
+    gpio_set_direction(LED_G, GPIO_MODE_OUTPUT);
+    gpio_set_direction(LED_B, GPIO_MODE_OUTPUT);
+
+    // Configurar ADC
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN_DB_11);
+
+    int ambiente, r, g, b;
+    int r_real, g_real, b_real;
+
+    while (1)
+    {
+        // 🔹 1. Ambiente (todo apagado)
+        gpio_set_level(LED_R, 0);
+        gpio_set_level(LED_G, 0);
+        gpio_set_level(LED_B, 0);
+        vTaskDelay(pdMS_TO_TICKS(200));
+
+        ambiente = adc1_get_raw(ADC_CHANNEL);
+
+        // 🔴 2. Rojo
+        gpio_set_level(LED_R, 1);
+        vTaskDelay(pdMS_TO_TICKS(200));
+        r = adc1_get_raw(ADC_CHANNEL);
+        gpio_set_level(LED_R, 0);
+
+        // 🟢 3. Verde
+        gpio_set_level(LED_G, 1);
+        vTaskDelay(pdMS_TO_TICKS(200));
+        g = adc1_get_raw(ADC_CHANNEL);
+        gpio_set_level(LED_G, 0);
+
+        // 🔵 4. Azul
+        gpio_set_level(LED_B, 1);
+        vTaskDelay(pdMS_TO_TICKS(200));
+        b = adc1_get_raw(ADC_CHANNEL);
+        gpio_set_level(LED_B, 0);
+
+        // 🔹 5. Valores reales
+        r_real = r - ambiente;
+        g_real = g - ambiente;
+        b_real = b - ambiente;
+
+        // 🔹 6. Mostrar resultados
+        printf("\n-----------\n");
+        printf("Ambiente: %d\n", ambiente);
+
+        printf("R: %d | V: %d | A: %d\n", r, g, b);
+        printf("R real: %d | V real: %d | A real: %d\n", r_real, g_real, b_real);
+
+        // 🔹 7. Detección corregida
+        int max = r_real;
+        int min = r_real;
+
+        if (g_real > max) max = g_real;
+        if (b_real > max) max = b_real;
+
+        if (g_real < min) min = g_real;
+        if (b_real < min) min = b_real;
+
+        if (r_real < 80 && g_real < 80 && b_real < 80)
+        {
+            printf("Resultado: SIN COLOR / MUY OSCURO\n");
+        }
+        else if ((max - min) < 250)
+        {
+            printf("Resultado: VERDE\n");
+        }
+        else if (r_real > b_real)
+        {
+            printf("Resultado: ROJO\n");
+        }
+        else
+        {
+            printf("Resultado: AZUL\n");
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
