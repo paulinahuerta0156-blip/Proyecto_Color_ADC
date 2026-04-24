c9bfbd5d4072a6e25cc32dd01f29a408d98d4fd0
Paulina Huerta <paulina.huerta0156@alumnos.udg.mx>
2026-04-24 01:20:25 -0600

    Avance 1: Sensor de color funcionando PERFECCIONADO

--- main/main.c
+++ main/main.c
@@ -9,7 +9,7 @@
 #define LED_G GPIO_NUM_17
 #define LED_B GPIO_NUM_18
 
-// ADC
+// ADC (GPIO 32)
 #define ADC_CHANNEL ADC1_CHANNEL_4
 
 void app_main(void)
@@ -28,7 +28,7 @@ void app_main(void)
 
     while (1)
     {
-        // 🔹 1. Ambiente (todo apagado)
+        //Ambiente (todo apagado)
         gpio_set_level(LED_R, 0);
         gpio_set_level(LED_G, 0);
         gpio_set_level(LED_B, 0);
@@ -36,37 +36,37 @@ void app_main(void)
 
         ambiente = adc1_get_raw(ADC_CHANNEL);
 
-        // 🔴 2. Rojo
+        //Rojo
         gpio_set_level(LED_R, 1);
         vTaskDelay(pdMS_TO_TICKS(200));
         r = adc1_get_raw(ADC_CHANNEL);
         gpio_set_level(LED_R, 0);
 
-        // 🟢 3. Verde
+        //Verde
         gpio_set_level(LED_G, 1);
         vTaskDelay(pdMS_TO_TICKS(200));
         g = adc1_get_raw(ADC_CHANNEL);
         gpio_set_level(LED_G, 0);
 
-        // 🔵 4. Azul
+        //Azul
         gpio_set_level(LED_B, 1);
         vTaskDelay(pdMS_TO_TICKS(200));
         b = adc1_get_raw(ADC_CHANNEL);
         gpio_set_level(LED_B, 0);
 
-        // 🔹 5. Valores reales
+        //Valores reales
         r_real = r - ambiente;
         g_real = g - ambiente;
         b_real = b - ambiente;
 
-        // 🔹 6. Mostrar resultados
+        //Mostrar resultados
         printf("\n-----------\n");
         printf("Ambiente: %d\n", ambiente);
 
         printf("R: %d | V: %d | A: %d\n", r, g, b);
         printf("R real: %d | V real: %d | A real: %d\n", r_real, g_real, b_real);
 
-        // 🔹 7. Detección corregida
+        //Detección (comparacion para definir max y min)
         int max = r_real;
         int min = r_real;
 
@@ -76,19 +76,20 @@ void app_main(void)
         if (g_real < min) min = g_real;
         if (b_real < min) min = b_real;
 
-        if (r_real < 80 && g_real < 80 && b_real < 80)
+        //Resultados
+        if (r_real < 80 && g_real < 80 && b_real < 80) //Umbral para cuando no hay objeto
         {
             printf("Resultado: SIN COLOR / MUY OSCURO\n");
         }
-        else if ((max - min) < 250)
+        else if ((max - min) < 250) //VERDE (diferencia porque asi funciona bien)
         {
             printf("Resultado: VERDE\n");
         }
-        else if (r_real > b_real)
+        else if (r_real > b_real) //ROJO
         {
             printf("Resultado: ROJO\n");
         }
-        else
+        else //AZUL
         {
             printf("Resultado: AZUL\n");
         }
