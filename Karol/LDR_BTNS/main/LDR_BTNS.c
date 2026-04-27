ef25c827fe0895d724ab27ccf8fcbdb16b662a68
KarolAraujoSaav <karol.araujo0147@alumnos.udg.mx>
2026-04-27 03:12:11 -0600

    Avance 5: Implementación del botón 2 de congelamiento

--- Karol/LDR_BTNS/main/LDR_BTNS.c
+++ Karol/LDR_BTNS/main/LDR_BTNS.c
@@ -5,20 +5,27 @@
 #include "driver/adc.h"
 
 // ===== DEFINICIÓN DE PINES =====
+
 #define LED_R GPIO_NUM_5
 #define LED_G GPIO_NUM_17
 #define LED_B GPIO_NUM_18
 
 #define BTN_1 GPIO_NUM_25   // Botón para cambiar modo.
+#define BTN_2 GPIO_NUM_13   // Congelar / descongelar.
 
 #define ADC_CHANNEL ADC1_CHANNEL_4  // GPIO32
 
 // ===== VARIABLES =====
 
 int R = 0, G = 0, B = 0;
-int modo = 0; // 0 = medición, 1 = clasificación.
 
-int estado_anterior = 1; // Para detectar flanco del botón.
+int modo = 0; // 0 = medición, 1 = clasificación.
+int congelado = 0;   // 0 = activo, 1 = congelado.
+
+// Variables para detectar flanco (botón).
+
+int estado_anterior_b1 = 1;
+int estado_anterior_b2 = 1;
 
 void app_main(void)
 {
@@ -33,11 +40,15 @@ void app_main(void)
     gpio_reset_pin(LED_B);
     gpio_set_direction(LED_B, GPIO_MODE_OUTPUT);
 
-    // ===== CONFIGURACIÓN DEL BOTÓN =====
+    // ===== CONFIGURACIÓN DE LOS BOTONES =====
     
     gpio_reset_pin(BTN_1);
     gpio_set_direction(BTN_1, GPIO_MODE_INPUT);
     gpio_set_pull_mode(BTN_1, GPIO_PULLUP_ONLY);
+    
+    gpio_reset_pin(BTN_2);
+    gpio_set_direction(BTN_2, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(BTN_2, GPIO_PULLUP_ONLY);
 
     // ===== CONFIGURACIÓN DEL ADC =====
     
@@ -46,22 +57,43 @@ void app_main(void)
 
     while (1)
     {
-        // ===============================
-        // LECTURA DEL BOTÓN
-        // ===============================
+        // ===================================
+        // LECTURA DEL BOTÓN 1: CAMBIAR MODO
+        // ===================================
         
         int b1 = gpio_get_level(BTN_1);
 
         // Detecta solo cuando se presiona.
         
-        if (b1 == 0 && estado_anterior == 1)
+        if (b1 == 0 && estado_anterior_b1 == 1)
         {
             modo = !modo;
             printf("CAMBIO DE MODO\n");
         }
 
-        estado_anterior = b1;
+        estado_anterior_b1 = b1;
 
+        // ===============================
+        // LECTURA DEL BOTÓN 2: CONGELAR
+        // ===============================
+        int b2 = gpio_get_level(BTN_2);
+
+        if (b2 == 0 && estado_anterior_b2 == 1)
+        {
+            congelado = !congelado;
+
+            if (congelado)
+                printf("MEDICION CONGELADA\n");
+            else
+                printf("MEDICION REANUDADA\n");
+        }
+
+        estado_anterior_b2 = b2;
+        
+        // Mediciones si no hay congelamiento.
+        
+         if (!congelado)
+         {
         // ===============================
         // MEDICIÓN CON LUZ ROJA
         // ===============================
@@ -105,7 +137,9 @@ void app_main(void)
         gpio_set_level(LED_R, 0);
         gpio_set_level(LED_G, 0);
         gpio_set_level(LED_B, 0);
-
+        
+        }
+        
         // ===============================
         // MOSTRAR SEGÚN MODO
         // ===============================
